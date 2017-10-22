90c3361b338f4f10f673615049b19585c134b4bf
outboxsol <outboxsol@gmail.com>
2017-10-22 21:17:43 +0530

    first commint

---
+++ updateFiles/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace updateFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.contractBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.filesDataSet = new updateFiles.filesDataSet();
+            this.contractTableAdapter = new updateFiles.filesDataSetTableAdapters.contractTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.contractBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filesDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "dropcontracts";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "updateDB";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.contractBindingSource;
+            this.comboBox1.DisplayMember = "instrument";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(40, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.ValueMember = "instrument";
+            // 
+            // contractBindingSource
+            // 
+            this.contractBindingSource.DataMember = "contract";
+            this.contractBindingSource.DataSource = this.filesDataSet;
+            // 
+            // filesDataSet
+            // 
+            this.filesDataSet.DataSetName = "filesDataSet";
+            this.filesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // contractTableAdapter
+            // 
+            this.contractTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 156);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.contractBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filesDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private filesDataSet filesDataSet;
+        private System.Windows.Forms.BindingSource contractBindingSource;
+        private filesDataSetTableAdapters.contractTableAdapter contractTableAdapter;
+    }
+}
+
---
+++ updateFiles/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace updateFiles
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DropContractTable();
+        }
+
+        //variables
+        public string mysqlconnectionstring = "Server=localhost;Database=files;Uid=root;Pwd=mysql;";
+        MySqlConnection connection;
+        string instrument;
+        int token;
+        public double count;
+        public double exceptioncount;
+        void readfile()
+        { 
+
+        string sourcepath = "d:\\contract.txt";
+        var source = System.IO.File.ReadAllLines(sourcepath);
+        
+            
+        for (int i = 1; i < source.Length; i++)
+        {
+
+            var sourcecells = source[i].Split('|');
+            string expirydate = gettimefromepochtime(Convert.ToDouble(sourcecells[6])).ToString("ddMMMyyyy").ToUpper();
+            //parse futstk
+
+            if ((expirydate == "30NOV2017") || (expirydate == "26OCT2017"))
+            {
+
+                if ((sourcecells[53] != "0" || sourcecells[0] != "0") && ((sourcecells[2] == "FUTSTK") || (sourcecells[2] == "FUTIDX")))
+                {
+
+                    //Debug.WriteLine(sourcecells[0] + "----" + sourcecells[53]);
+
+                    string instrument = (sourcecells[2] + "-" + sourcecells[3] + "-" + expirydate).ToUpper();
+                    insertData(instrument, sourcecells[0]);
+                }
+
+                // parsing for OPTSTK
+                if ((sourcecells[53] != "0" || sourcecells[0] != "0") && ((sourcecells[2] == "OPTSTK") || (sourcecells[2] == "OPTIDX")))
+                {
+
+
+                    //FORMAT OPTSTK-SYMOL_NAME-DDMMMYYYY-STRIKEPRICE-OPTIONTYPE
+
+                    string strikePrice = Convert.ToString((Convert.ToInt64(sourcecells[7]) / 100).ToString("0.00"));
+
+                    string optionType = sourcecells[8];
+                    string instrument = (sourcecells[2] + "-" + sourcecells[3] + "-" + expirydate).ToUpper() + "-" + strikePrice + "-" + optionType;
+                    System.Diagnostics.Debug.WriteLine(instrument);
+                    insertData(instrument, sourcecells[0]);
+                    int X = 0;
+                }
+    }
+
+            
+
+        }
+        MessageBox.Show("Total Count = " + count + "\nException count = " + exceptioncount);
+
+        }
+
+
+        DateTime  gettimefromepochtime(double timestamp)
+        {
+           
+
+            // First make a System.DateTime equivalent to the UNIX Epoch.
+            System.DateTime dateTime = new System.DateTime(1980, 1, 1, 0, 0, 0, 0);
+
+            // Add the number of seconds in UNIX timestamp to be converted.
+            return dateTime = dateTime.AddSeconds(timestamp).ToLocalTime();
+            
+        }
+
+        void insertData(string instrument, string token)
+        {
+
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(mysqlconnectionstring);
+                if (connection.State == ConnectionState.Broken || connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                    System.Diagnostics.Debug.WriteLine("Connection state was : " + connection.State + " hence opened " + "\n" + " count : = " + count + "Exception count = " + exceptioncount);
+                }
+                MySqlCommand command = connection.CreateCommand();
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO contract(instrument,token) VALUES (@instrument,@token)";
+                 
+                command.Prepare();
+                //----------------*********************---------------------
+                command.Parameters.AddWithValue("@instrument", instrument);
+                command.Parameters.AddWithValue("@token", token);
+                
+                //----------------*********************---------------------
+                command.ExecuteNonQuery();
+                connection.Close();//have to come up with logic to close it after all the contracts are deleted
+                count++;
+              
+            }
+            catch (MySqlException mysql)
+            {
+                exceptioncount++;
+                System.Diagnostics.Debug.WriteLine("Mysql exception :- " + mysql.ToString());
+            }
+        }
+
+        void DropContractTable()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(mysqlconnectionstring);
+
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                MySqlCommand command = connection.CreateCommand();
+                command.Connection = connection;
+                command.CommandText = "DELETE FROM files.contract;";
+                command.ExecuteNonQuery();
+                connection.Clone();
+
+            }
+            catch (MySqlException mysql)
+            {
+
+                MessageBox.Show("DropContractTable" + "::" + mysql.Message.ToString());
+            }
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readfile();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'filesDataSet.contract' table. You can move, or remove it, as needed.
+            this.contractTableAdapter.Fill(this.filesDataSet.contract);
+
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.contractTableAdapter.FillBy(this.filesDataSet.contract);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+
+
+    }
+}
